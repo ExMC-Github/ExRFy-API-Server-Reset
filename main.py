66c3c31fc39b8574bb3da2b1e069b2823313ee55
ExRFy158 <2545869165@qq.com>
2026-02-18 15:13:38 +0800

    Icon

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
-def print_hi():
+def index():
     return '<html><head><title>主页</title></head>网页正在运行<br><br><footer>Powered By <a href="https://github.com/ExMC-Github/ExRFy-API-Server-Reset">ExRFy API Reset</a></footer></html>'
 
-
+@app.route("/favicon.ico")
+def icon():
+    try:
+        file = open("favicon.ico", 'rb').read()
+        return file
+    except:
+        return "Not Found",404
 if __name__ == '__main__':
     app.run()
 
